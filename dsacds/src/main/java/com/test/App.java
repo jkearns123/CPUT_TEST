27d05677d1be60e688bf43e2b298991126ec0991
jkearns123 <jkearns123@gmail.com>
2019-02-12 23:10:48 +0200

    Updated java file

--- dsacds/src/main/java/com/test/App.java
+++ dsacds/src/main/java/com/test/App.java
@@ -6,10 +6,20 @@ package com.test;
  */
 public class App 
 {
+
+
     public static void main( String[] args )
     {
         System.out.print("THIS IS A TEST");
         System.out.print("THIS IS A TEST FROM ALDADO");
         System.out.print("Edit done by Ryan");
+
+        int x = 0;
+
+        while (x <= 4){
+            System.out.println("Jade Kearns 205074553");
+
+            x++;
+        }
     }
 }
