30049405bc9448f9bf01082824c970955f78ba84
Darren <Darren.Allen1985@gmail.com>
2019-02-12 19:25:39 +0200

    First commit

---
+++ dsacds/src/test/java/com/test/AppTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import static org.junit.Assert.assertTrue;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+import org.junit.Test;
+
+import javax.swing.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null,"This is a test1");
+        JOptionPane.showMessageDialog(null,"This is a test2");
+    }
+}
